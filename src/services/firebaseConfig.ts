5019b590b471a983983e0702162830b4c281bdc6
Fabsolute33 <loclacanau33@gmail.com>
2026-01-08 16:23:55 +0100

    Update firebaseConfig and workspace settings

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAB0Z0xNQ5cGhCYxZ-r-IPHPU7gQ7p-QMg",
-    authDomain: "architecte-seo.firebaseapp.com",
-    projectId: "architecte-seo",
-    storageBucket: "architecte-seo.firebasestorage.app",
-    messagingSenderId: "359132203830",
-    appId: "1:359132203830:web:231df9a2d4c7553bf04197"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
